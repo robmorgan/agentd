e32df04df01d8719af945d2931c6337a4ea4d4cc
Rob Morgan <robbym@gmail.com>
2026-03-27 10:58:41 +0800

    feat: add terminal mockup

---
+++ mockup.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+echo "
+❯ agent ls
+
+  RUN   Δ   ↑   AGE   NAME            BRANCH
+  \x1b[38;5;46m●\x1b[0m     -   -   \x1b[2m9h\x1b[0m    \x1b[1mcalm-foxes\x1b[0m      \x1b[2magent/calm-foxes\x1b[0m
+  \x1b[38;5;240m○\x1b[0m     \x1b[38;5;220m3\x1b[0m   -   \x1b[2m9h\x1b[0m    \x1b[1mbrisk-pandas\x1b[0m    \x1b[2magent/brisk-pandas\x1b[0m
+  \x1b[38;5;51m✓\x1b[0m     -   \x1b[38;5;213m2\x1b[0m   \x1b[2m9h\x1b[0m    \x1b[1mremove-create\x1b[0m   \x1b[2magent/remove-create\x1b[0m
+  \x1b[38;5;196m✖\x1b[0m     \x1b[38;5;220m5\x1b[0m   -   \x1b[2m19m\x1b[0m   \x1b[1mswift-geckos\x1b[0m    \x1b[2magent/swift-geckos\x1b[0m"
+
+echo "
+❯ agent ls
+
+  RUN   Δ   ↑   AGE   NAME            BRANCH
+  \x1b[38;5;46m●\x1b[0m     -   -   \x1b[2m9h\x1b[0m    \x1b[1mcalm-foxes\x1b[0m      \x1b[2magent/calm-foxes\x1b[0m
+  \x1b[48;5;235m\x1b[38;5;240m○\x1b[0m     \x1b[38;5;220m3\x1b[0m   -   \x1b[2m9h\x1b[0m    \x1b[1mbrisk-pandas\x1b[0m    \x1b[2magent/brisk-pandas\x1b[0m\x1b[0m
+  \x1b[38;5;51m✓\x1b[0m     -   \x1b[38;5;213m2\x1b[0m   \x1b[2m9h\x1b[0m    \x1b[1mremove-create\x1b[0m   \x1b[2magent/remove-create\x1b[0m
+  \x1b[48;5;52m\x1b[38;5;196m✖\x1b[0m     \x1b[38;5;220m5\x1b[0m   -   \x1b[2m19m\x1b[0m   \x1b[1mswift-geckos\x1b[0m    \x1b[2magent/swift-geckos\x1b[0m\x1b[0m
+  "
